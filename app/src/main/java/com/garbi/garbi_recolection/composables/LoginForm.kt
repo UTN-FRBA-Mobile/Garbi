a72d036d5d6fcf77603b70523844178ce2ac73d9
nacho <nachoengineeringdev@gmail.com>
2024-05-07 22:09:35 -0300

    added new IU to login page

--- app/src/main/java/com/garbi/garbi_recolection/composables/LoginForm.kt
+++ app/src/main/java/com/garbi/garbi_recolection/composables/LoginForm.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -25,71 +27,85 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.garbi.garbi_recolection.MainActivity
+import com.garbi.garbi_recolection.R
 import com.garbi.garbi_recolection.fields.LoginField
 import com.garbi.garbi_recolection.fields.PasswordField
 import com.garbi.garbi_recolection.models.Credentials
 import com.garbi.garbi_recolection.ui.theme.Garbi_recolectionTheme
 import com.garbi.garbi_recolection.ui.theme.Green900
 
+@Preview
 @Composable
 fun LoginForm(navController: NavController? = null) {
     Garbi_recolectionTheme {
         Surface {
             var credentials by remember { mutableStateOf(Credentials()) }
             val context = LocalContext.current
-
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 30.dp)
-            ) {
-                LoginField(
-                    value = credentials.login,
-                    onChange = { data -> credentials = credentials.copy(login = data) },
+            Column {
+                Spacer(modifier = Modifier.height(40.dp))
+                Image(
+                    painter = painterResource(id = R.mipmap.garbi_logo_recortado),
+                    contentDescription = "Garbi logo",
+                    contentScale = ContentScale.FillWidth,
                     modifier = Modifier.fillMaxWidth()
                 )
-                PasswordField(
-                    value = credentials.pwd,
-                    onChange = { data -> credentials = credentials.copy(pwd = data) },
-                    submit = {
-                        if (!checkCredentials(credentials, context)) credentials = Credentials()
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                Spacer(modifier = Modifier.height(10.dp))
-                LabeledCheckbox(
-                    label = "Quiero recordar mi usuario",
-                    onCheckChanged = {
-                        credentials = credentials.copy(remember = !credentials.remember)
-                    },
-                    isChecked = credentials.remember
-                )
-                Spacer(modifier = Modifier.height(20.dp))
-                Button(
-                    onClick = {
-                        // if (!checkCredentials(credentials, context)) credentials = Credentials()
-                        if (navController != null) {
-                            navController.navigate("home")
-                        }
-                    },
-                    enabled = credentials.isNotEmpty(),
-                    shape = RoundedCornerShape(5.dp),
-                    modifier = Modifier.fillMaxWidth(),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.White,
-                        containerColor = Green900
-                    )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 30.dp)
                 ) {
-                    Text("Inciar sesión")
+
+                    Spacer(modifier = Modifier.height(100.dp))
+                    LoginField(
+                        value = credentials.login,
+                        onChange = { data -> credentials = credentials.copy(login = data) },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(10.dp))
+                    PasswordField(
+                        value = credentials.pwd,
+                        onChange = { data -> credentials = credentials.copy(pwd = data) },
+                        submit = {
+                            if (!checkCredentials(credentials, context)) credentials = Credentials()
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(12.dp))
+                    LabeledCheckbox(
+                        label = "Quiero recordar mi usuario",
+                        onCheckChanged = {
+                            credentials = credentials.copy(remember = !credentials.remember)
+                        },
+                        isChecked = credentials.remember,
+
+                        )
+                    Spacer(modifier = Modifier.height(30.dp))
+                    Button(
+                        onClick = {
+                            // if (!checkCredentials(credentials, context)) credentials = Credentials()
+                            if (navController != null) {
+                                navController.navigate("home")
+                            }
+                        },
+                        enabled = credentials.isNotEmpty(),
+                        shape = RoundedCornerShape(5.dp),
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = ButtonDefaults.buttonColors(
+                            contentColor = Color.White,
+                            containerColor = Green900
+                        )
+                    ) {
+                        Text("Inciar sesión")
+                    }
                 }
             }
         }
@@ -102,13 +118,11 @@ fun LabeledCheckbox(
     onCheckChanged: () -> Unit,
     isChecked: Boolean
 ) {
-
     Row(
         Modifier
             .clickable(
                 onClick = onCheckChanged
             )
-            .padding(4.dp)
     ) {
         Checkbox(checked = isChecked, onCheckedChange = null)
         Spacer(Modifier.size(6.dp))
