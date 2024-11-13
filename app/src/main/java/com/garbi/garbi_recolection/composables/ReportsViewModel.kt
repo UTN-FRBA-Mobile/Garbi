ebfc83ceb2027048e7b0006f08ed68e2451c6fa7
LemmeGuadalupe <guadalupe.a.lemme@gmail.com>
2024-11-12 21:33:11 -0300

    fix: filter reports

--- app/src/main/java/com/garbi/garbi_recolection/composables/ReportsViewModel.kt
+++ app/src/main/java/com/garbi/garbi_recolection/composables/ReportsViewModel.kt
@@ -52,7 +52,7 @@ class ReportsViewModel : ViewModel() {
             Log.v("reportes", "response ${response}")
             if(response.body()?.result?.isNotEmpty() == true){
                 Log.v("reportes","hay reportes ${response.body()}")
-                reports = response.body()!!.result//.filter { it.deletedAt == null }
+                reports = response.body()!!.result.filter { it.userId !== null }
             }else{
                 Log.v("reportes","no hay reportes")
             }
